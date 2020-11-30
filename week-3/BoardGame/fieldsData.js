fc83cad8d4dfe8c447a1c14d73f8112494f2adf2
Nadya Georgieva <44587584+Nadya041@users.noreply.github.com>
2020-11-30 11:20:01 +0200

    field fix

---
+++ week-3/BoardGame/fieldsData.js
@@ -0,0 +1,122 @@
+const availableFields = [
+    {
+        name: "START"
+
+    },
+
+    {
+        name: "Ramada Trimontium",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Mall Markovo Tepe",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Raffy Bar Gelato",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Mall Plaza",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "W-garden",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Happy Bar and Grill",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Bushido",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Moda Bar Escape",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Secrets",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Ememento",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Osteria Rosmarino",
+        price: 500,
+        tax: 60,
+        owner: null
+    },
+    {
+        name: "Porto Greco",
+        price: 600,
+        tax: 70,
+        owner: null
+    },
+    {
+        name: "Planet Club",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Mall Plovdiv",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Afreddo ",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "McDonald's",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Vision Club",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Soho Day and Night",
+        price: 250,
+        tax: 20,
+        owner: null
+    },
+    {
+        name: "Megami",
+        price: 250,
+        tax: 20,
+        owner: null
+    }
+
+];
--- week-3/BoardGame/script.js
+++ week-3/BoardGame/script.js
@@ -1,78 +1,62 @@
-let box1 = document.getElementById("box1");
-let box2 = document.getElementById("box2");
-let box3 = document.getElementById("box3");
-let box4 = document.getElementById("box4");
-let box5 = document.getElementById("box5");
-let box6 = document.getElementById("box6");
-let box7 = document.getElementById("box7");
-let box8 = document.getElementById("box8");
-let box9 = document.getElementById("box9");
-let box10 = document.getElementById("box10");
-let box11 = document.getElementById("box11");
-let box12 = document.getElementById("box12");
-let box13 = document.getElementById("box13");
-let box14 = document.getElementById("box14");
-let box15 = document.getElementById("box15");
-let box16 = document.getElementById("box16");
-let box17 = document.getElementById("box17");
-let box18 = document.getElementById("box18");
-let box19 = document.getElementById("box19");
-let box20 = document.getElementById("box20");
+let myBoardElement = document.getElementById("myBoard");
 
 const GameEngine = {
     gameFields: [],
-    availableFields: [
-        "TRAP", "TRAP", "TRAP", "TRAP", "TRAP", "TRAP", "TRAP",
-        "Invest", "Invest", "Invest",
-        "Party hard", "Party hard", "Party hard",
-        "Chance", "Chance", "Chance",
-        "Steal", "Steal", "Steal"
-    ],
+    availableFields: [...availableFields],
+
     drawBoard() {
-        box1.innerHTML = this.gameFields[0];
-        box2.innerHTML = this.gameFields[1];
-        box3.innerHTML = this.gameFields[2];
-        box4.innerHTML = this.gameFields[3];
-        box5.innerHTML = this.gameFields[4];
-        box6.innerHTML = this.gameFields[5];
-        box7.innerHTML = this.gameFields[6];
-        box8.innerHTML = this.gameFields[7];
-        box9.innerHTML = this.gameFields[8];
-        box10.innerHTML = this.gameFields[9];
-        box11.innerHTML = this.gameFields[10];
-        box12.innerHTML = this.gameFields[11];
-        box13.innerHTML = this.gameFields[12];
-        box14.innerHTML = this.gameFields[13];
-        box15.innerHTML = this.gameFields[14];
-        box16.innerHTML = this.gameFields[15];
-        box17.innerHTML = this.gameFields[16];
-        box18.innerHTML = this.gameFields[17];
-        box19.innerHTML = this.gameFields[18];
-        box20.innerHTML = this.gameFields[19];
-
 
     },
+    getBoxTemplate: (fieldInfo) => {
+        return `<div class = "box">
+         
+      <spam> ${fieldInfo.name} </spam>
+      <br> <spam> price: ${fieldInfo.price} </spam>
+      <br> <spam> tax: ${fieldInfo.tax} </spam>
+     
+      </div>`;
+    },
+
     generateBoard() {
-        this.gameFields.push("START");
-        let cloneAvailableFields = [...this.availableFields];
+        let template = [`
+        <div class  = "first-row row" >
+        <div class = "box"> ${availableFields[0].name} </div>
 
-        while (cloneAvailableFields.length > 0) {
-            var item = cloneAvailableFields[Math.floor(Math.random() * cloneAvailableFields.length)];
-            this.gameFields.push(item);
-            const index = cloneAvailableFields.indexOf(item);
-            cloneAvailableFields.splice(index, 1);
+      ` ]
+        for (let i = 1; i < 8; i++) {
+            template.push(this.getBoxTemplate(availableFields[i]));
         }
-
+        template.push(" </div> ");
+        template.push(`<div class = "second-row row">
+      
+      `)
+        for (let i = 8; i < 10; i++) {
+            template.push(this.getBoxTemplate(availableFields[i]));
+        }
+        template.push(" </div> ");
+        template.push(`<div class = "third-row row">
+      
+      `)
+        for (let i = 10; i < 12; i++) {
+            template.push(this.getBoxTemplate(availableFields[i]));
+        }
+        template.push(" </div> ");
+        template.push(`<div class = "last-row row">
+      
+      `)
+        for (let i = 12; i < 20; i++) {
+            template.push(this.getBoxTemplate(availableFields[i]));
+        }
+        template.push(" </div> ");
+        myBoardElement.innerHTML = template.join("");
 
     },
 
-    throwDice: (sideCount) =>{
-        return Math.floor(Math.random() * Math.floor(sideCount));
-
-            
+    throwDice: (sideCount) => {
+        return Math.floor(Math.random() * Math.floor(sideCount)) + 1;
     }
 }
 GameEngine.generateBoard();
 GameEngine.drawBoard();
 
-console.log(GameEngine.throwDice(8));
+console.log(GameEngine.throwDice(6));

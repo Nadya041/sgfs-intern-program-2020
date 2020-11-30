5c5ffb83649b89143bdf8e5745d65e40b0884da9
Nadya Georgieva <44587584+Nadya041@users.noreply.github.com>
2020-12-01 00:57:27 +0200

    players movement

--- week-3/BoardGame/fieldsData.js
+++ week-3/BoardGame/fieldsData.js
@@ -1,122 +1,158 @@
 const availableFields = [
     {
+        id : 0,
         name: "START"
-
     },
 
     {
+        id : 1,
         name: "Ramada Trimontium",
-        price: 250,
+        price: 330,
         tax: 20,
         owner: null
     },
     {
+        id : 2,
         name: "Mall Markovo Tepe",
-        price: 250,
-        tax: 20,
+        price: 620,
+        tax: 50,
         owner: null
     },
     {
+        id : 3,
         name: "Raffy Bar Gelato",
-        price: 250,
-        tax: 20,
+        price: 480,
+        tax: 30,
         owner: null
     },
     {
+        id : 4,
         name: "Mall Plaza",
-        price: 250,
-        tax: 20,
+        price: 760,
+        tax: 55,
         owner: null
     },
     {
+        id : 5,
         name: "W-garden",
-        price: 250,
-        tax: 20,
+        price: 500,
+        tax: 35,
         owner: null
     },
     {
+        id : 6,
         name: "Happy Bar and Grill",
-        price: 250,
-        tax: 20,
+        price: 420,
+        tax: 42,
         owner: null
     },
     {
+        id : 7,
         name: "Bushido",
-        price: 250,
-        tax: 20,
+        price: 600,
+        tax: 35,
         owner: null
     },
     {
+        id : 19,
         name: "Moda Bar Escape",
         price: 250,
         tax: 20,
         owner: null
     },
     {
+        id : 8,
         name: "Secrets",
-        price: 250,
-        tax: 20,
+        price: 360,
+        tax: 25,
         owner: null
     },
     {
+        id : 18,
         name: "Ememento",
-        price: 250,
-        tax: 20,
-        owner: null
-    },
-    {
-        name: "Osteria Rosmarino",
-        price: 500,
+        price: 720,
         tax: 60,
         owner: null
     },
     {
+        id : 9,
+        name: "Osteria Rosmarino",
+        price: 500,
+        tax: 45,
+        owner: null
+    },
+    {
+        id : 17,
         name: "Porto Greco",
         price: 600,
         tax: 70,
         owner: null
     },
     {
+        id : 16,
         name: "Planet Club",
-        price: 250,
-        tax: 20,
+        price: 750,
+        tax: 40,
         owner: null
     },
     {
+        id : 15,
         name: "Mall Plovdiv",
-        price: 250,
-        tax: 20,
+        price: 800,
+        tax: 60,
         owner: null
     },
     {
+        id : 14,
         name: "Afreddo ",
-        price: 250,
-        tax: 20,
+        price: 490,
+        tax: 45,
         owner: null
     },
     {
+        id : 13,
         name: "McDonald's",
-        price: 250,
-        tax: 20,
+        price: 360,
+        tax: 15,
         owner: null
     },
     {
+        id : 12,
         name: "Vision Club",
-        price: 250,
-        tax: 20,
+        price: 500,
+        tax: 45,
         owner: null
     },
     {
+        id : 11,
         name: "Soho Day and Night",
-        price: 250,
-        tax: 20,
+        price: 740,
+        tax: 62,
         owner: null
     },
     {
+        id : 10,
         name: "Megami",
-        price: 250,
-        tax: 20,
+        price: 520,
+        tax: 40,
         owner: null
     }
 
 ];
+
+
+const availablePlayerCollection = [
+    {
+        id          : 1, 
+        name        : "Nadya",
+        activeBox   : 0,
+        wallet      : 1000
+    },
+
+    {
+        id          : 2, 
+        name        : "Joro",
+        activeBox   : 0,
+        wallet      : 1000
+    }
+]
--- week-3/BoardGame/script.js
+++ week-3/BoardGame/script.js
@@ -1,54 +1,99 @@
 let myBoardElement = document.getElementById("myBoard");
+let playerInfoTurn = document.getElementById("player-info-box");
+let diceInfo = document.getElementById("dice-info");
+let throwDice = document.getElementById("throw-dice--action");
+
+throwDice.addEventListener("click", function () {
+
+    GameEngine.processDiceThrow();
+});
 
 const GameEngine = {
     gameFields: [],
     availableFields: [...availableFields],
+    players: [...availablePlayerCollection],
+    currentPlayerIndex: 0,
+
+
+    showPlayerTurn() {
+        playerInfoTurn.innerHTML = ("The player on turn is: " + GameEngine.players[this.currentPlayerIndex].name)
+
+    },
+
+    processDiceThrow() {
+        let currenDiceThrow = GameEngine.throwDice(6);
+        diceInfo.innerHTML = ("You throw : " + currenDiceThrow);
+        GameEngine.players[this.currentPlayerIndex].activeBox += currenDiceThrow;
+        if (GameEngine.players[this.currentPlayerIndex].activeBox >= this.availableFields.length) {
+            GameEngine.players[this.currentPlayerIndex].activeBox -= this.availableFields.length;
+        }
+
+       this.currentPlayerIndex = this.currentPlayerIndex == 0 ?1:0;
+
+        GameEngine.showPlayerTurn()
+
+        this.generateBoard();
+
+    },
+
+
 
     drawBoard() {
 
     },
     getBoxTemplate: (fieldInfo) => {
-        return `<div class = "box">
-         
-      <spam> ${fieldInfo.name} </spam>
-      <br> <spam> price: ${fieldInfo.price} </spam>
-      <br> <spam> tax: ${fieldInfo.tax} </spam>
-     
-      </div>`;
+
+        let template = [`<div class = "box"><span> ${fieldInfo.name}</span>`]
+        if (fieldInfo.id != 0) {
+            template.push(`<br> <span> price: ${fieldInfo.price} </span>
+            <br> <span> tax: ${fieldInfo.tax} </span>`);
+        }
+
+        for (let i = 0; i < GameEngine.players.length; i++) {
+            if
+                (GameEngine.players[i].activeBox == fieldInfo.id) {
+                template.push(`<span> <br> ${GameEngine.players[i].name} </span>`)
+
+            }
+        }
+
+        template.push(" </div>");
+        return template.join("");
+
+    },
+
+    buildGameBoardRow(startCol, endCol, cssClass) {
+
+        let template = [];
+        template.push(`<div class = "${cssClass} row">
+      
+        `)
+        for (let i = startCol; i < endCol; i++) {
+            template.push(this.getBoxTemplate(this.availableFields[i]));
+        }
+        template.push(" </div> ");
+
+        return template;
     },
 
     generateBoard() {
-        let template = [`
-        <div class  = "first-row row" >
-        <div class = "box"> ${availableFields[0].name} </div>
 
-      ` ]
-        for (let i = 1; i < 8; i++) {
-            template.push(this.getBoxTemplate(availableFields[i]));
-        }
-        template.push(" </div> ");
-        template.push(`<div class = "second-row row">
-      
-      `)
-        for (let i = 8; i < 10; i++) {
-            template.push(this.getBoxTemplate(availableFields[i]));
-        }
-        template.push(" </div> ");
-        template.push(`<div class = "third-row row">
-      
-      `)
-        for (let i = 10; i < 12; i++) {
-            template.push(this.getBoxTemplate(availableFields[i]));
-        }
-        template.push(" </div> ");
-        template.push(`<div class = "last-row row">
-      
-      `)
-        for (let i = 12; i < 20; i++) {
-            template.push(this.getBoxTemplate(availableFields[i]));
-        }
-        template.push(" </div> ");
+        let template = this.buildGameBoardRow(0, 8, "first-row")
+        template.push(...this.buildGameBoardRow(8, 10, "second-row"));
+        template.push(...this.buildGameBoardRow(10, 12, "third-row"));
+        template.push(...this.buildGameBoardRow(12, 20, "last-row"));
+
         myBoardElement.innerHTML = template.join("");
+    },
+
+    startGame() {
+        this.availableFields = [...availableFields]
+        this.players = [...availablePlayerCollection]
+        this.generateBoard();
+        this.showPlayerTurn();
+
+
+
 
     },
 
@@ -56,7 +101,6 @@ const GameEngine = {
         return Math.floor(Math.random() * Math.floor(sideCount)) + 1;
     }
 }
-GameEngine.generateBoard();
-GameEngine.drawBoard();
+GameEngine.startGame();
 
-console.log(GameEngine.throwDice(6));
+
